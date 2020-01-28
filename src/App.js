41103180999b18815f8239b331c82ef8ff3a5358
rudalson <rudalson@gmail.com>
2020-01-28 19:30:50 +0900

    added the 06. 여기서 state++ 하면 에러가 난다. 왜 날까?

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            number: 49
+        }
+    }
+
+    onClickhandler() {
+        console.log(this);
+        this.state.number++;
+    }
+
     render() {
         return (
             <div className="App">
+                <Display value={this.state.number}/>
+                <div>
+                    <button onClick={this.onClickhandler}>증가버튼</button>
+                </div>
+                <div>
+                    <button>감소버튼</button>
+                </div>
             </div>
         );
     }
 }
+
+const Display = ({value}) =>
+    <div>
+        <h1>{value}</h1>
+    </div>
 
 export default App;
