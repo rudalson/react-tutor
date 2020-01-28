0c647a69e2317113106e7012ab6f6c24d528bf39
rudalson <rudalson@gmail.com>
2020-01-28 19:20:59 +0900

    added the 06. 6차시 카운터 만들기(state의 갱신, bind, Arrow function) 를 하기 위해 사전 준비

--- src/App.js
+++ src/App.js
@@ -1,53 +1,12 @@
 import React from 'react';
-import './App.css';
-import Person2 from "./Person2"
-import {Person3} from "./Person3"
 
 class App extends React.Component {
-    state = {
-        person: [
-            {name: "홍길동", age: 26},
-            {name: "고길동", age: 46},
-            {name: "머스크", age: 36},
-        ]
-    }
-
-    // render() {
-    //     return (
-    //         <div className="App">
-    //             <Person2 name={this.state.person[0].name} age={this.state.person[0].age}/>
-    //             <Person2 name={this.state.person[1].name} age={this.state.person[1].age}/>
-    //             <Person3 name={"이순신"} age={35}/>
-    //         </div>
-    //     );
-    // }
-
     render() {
-        const {person} = this.state;
-        const myfun = () => {
-            console.log("test")
-        };
         return (
             <div className="App">
-                <Person2
-                    name={"강감찬"}
-                    age={45}
-                    myfun={myfun}
-                    st={this.state}
-                />
             </div>
         );
     }
 }
-
-/*
-function Person() {
-    return <h1>첫번째 컴포넌트 입니다</h1>
-}
-/*/
-const Person = () => {
-    return <h1>첫번째 컴포넌트 입니다</h1>
-}
-//*/
 
 export default App;
