eeebfefe06e6a25eccf6859c6d96d4690e790755
rudalson <rudalson@gmail.com>
2020-01-31 02:58:23 +0900

    added 9차시 ToDo App 만들기 - 1 (React-firebase 연동)
    tasks에 task가 포함되고 출력을 하도록 구현. 하지만 동적으로 출력시키진 못한다.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 class App extends React.Component {
     state = {
-        tasks: [],
+        tasks: [
+            {todo: '할일 1'},
+            {todo: '할일 2'},
+        ],
         task: ''
     };
 
     onClickHandler = (e) => {
-        console.log(e.target);
+        const task = {todo: this.state.task};
+        this.setState({
+            tasks: [task],
+            task: ''
+        });
+        console.log(this.state.tasks);
     };
 
     onChangeHandler = (e) => {
@@ -22,6 +30,10 @@ class App extends React.Component {
                 <div>
                     <input value={this.state.task} onChange={this.onChangeHandler}></input>
                     <button onClick={this.onClickHandler}>저장</button>
+                </div>
+                <div>
+                    <p>{this.state.tasks[0]['todo']}</p>
+                    <button>delete</button>
                 </div>
             </div>
         );
