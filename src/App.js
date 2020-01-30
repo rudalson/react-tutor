91067c9bf705aa96972886350a8a540b5d458be4
rudalson <rudalson@gmail.com>
2020-01-31 02:47:37 +0900

    added 9차시 ToDo App 만들기 - 1 (React-firebase 연동)
    input창, button 그리고 state간 기본 코드 형태

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = {
+        tasks: [],
+        task: ''
+    };
+
+    onClickHandler = (e) => {
+        console.log(e.target);
+    };
+
+    onChangeHandler = (e) => {
+        this.setState({
+            task: e.target.value
+        })
+    };
+
     render() {
         return (
             <div className="App">
+                <div>
+                    <input value={this.state.task} onChange={this.onChangeHandler}></input>
+                    <button onClick={this.onClickHandler}>저장</button>
+                </div>
             </div>
         );
     }
